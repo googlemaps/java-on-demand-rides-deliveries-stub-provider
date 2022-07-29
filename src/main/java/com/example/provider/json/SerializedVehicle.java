7d9542144c0fed9a938fa1e9eca93f99aed1b9d5
Lucy Sun <jiaqinsun@google.com>
2022-07-29 13:39:49 -0700

    Added SerializedLastLocation, modified SerializedVehicle and VehicleSerializer.
    Co-authored-by: Isaac Song <isaacsong@google.com>

--- src/main/java/com/example/provider/json/SerializedVehicle.java
+++ src/main/java/com/example/provider/json/SerializedVehicle.java
@@ -1,25 +1,30 @@
 /* Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.example.provider.json;
 
 import com.google.auto.value.AutoValue;
+import com.google.type.LatLng;
+
 import google.maps.fleetengine.v1.TripType;
+import google.maps.fleetengine.v1.VehicleAttribute;
+import google.maps.fleetengine.v1.Vehicle.VehicleType;
 import java.util.List;
 
 /**
- * Vehicle object to serialize to clients. This object will only contain relevant vehicle
+ * Vehicle object to serialize to clients. This object will only contain
+ * relevant vehicle
  * information to send to the clients.
  */
 @AutoValue
@@ -27,18 +32,35 @@ abstract class SerializedVehicle {
 
   /** Name of the vehicle. */
   abstract String name();
+
   /** Current state of the vehicle given by fleetengine. */
   abstract String vehicleState();
-  /** List of remanining waypoints. Ex: Waypoints that the vehicle will visit next. */
+
+  /**
+   * List of remanining waypoints. Ex: Waypoints that the vehicle will visit next.
+   */
   abstract List<Waypoint> waypoints();
+
   /** List of assigned trip Ids for vehicle */
   abstract List<String> currentTripsIds();
+
   /** Back to back enabled */
   abstract boolean backToBackEnabled();
+
   /** Maximum capacity */
   abstract int maximumCapacity();
+
   /** Supported trip types */
   abstract List<TripType> supportedTripTypes();
+
+  /** Last location of vehicle */
+  abstract SerializedLastLocation lastLocation();
+
+  /** Vehicle attributes */
+  abstract List<VehicleAttribute> vehicleAttributes();
+
+  /** Vehicle type */
+  abstract VehicleType vehicleType();
 
   static Builder newBuilder() {
     return new AutoValue_SerializedVehicle.Builder();
@@ -96,6 +118,27 @@ abstract class SerializedVehicle {
      * @param supportedTripTypes list of supported trip types.
      */
     abstract Builder setSupportedTripTypes(List<TripType> supportedTripTypes);
+
+    /**
+     * Setter for the list of supported trip types for the vehicle.
+     *
+     * @param supportedTripTypes list of supported trip types.
+     */
+    abstract Builder setLastLocation(SerializedLastLocation lastLocation);
+
+    /**
+     * Setter for the list of supported trip types for the vehicle.
+     *
+     * @param supportedTripTypes list of supported trip types.
+     */
+    abstract Builder setVehicleAttributes(List<VehicleAttribute> vehicleAttributes);
+
+    /**
+     * Setter for the list of supported trip types for the vehicle.
+     *
+     * @param supportedTripTypes list of supported trip types.
+     */
+    abstract Builder setVehicleType(VehicleType vehicleType);
 
     abstract SerializedVehicle build();
   }
--- src/main/java/com/example/provider/json/VehicleSerializer.java
+++ src/main/java/com/example/provider/json/VehicleSerializer.java
@@ -1,17 +1,17 @@
 /* Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.example.provider.json;
 
 import static java.util.stream.Collectors.toList;
@@ -21,6 +21,8 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
+import com.google.protobuf.Int32Value;
+
 import google.maps.fleetengine.v1.Vehicle;
 import java.lang.reflect.Type;
 import java.util.List;
@@ -28,36 +30,57 @@ import java.util.List;
 /**
  * Serializer for vehicle object to provide relevant information to its clients.
  *
- * <p>From the fleetengine vehicle object, this serializer retrieves the name and the vehicleState.
+ * <p>
+ * From the fleetengine vehicle object, this serializer retrieves the name and
+ * the vehicleState.
  */
 final class VehicleSerializer implements JsonSerializer<Vehicle> {
 
+  Int32Value kDefaultInt32Value = Int32Value.newBuilder()
+      .setValue(0)
+      .build();
+
   @Override
   public JsonElement serialize(Vehicle src, Type typeOfSrc, JsonSerializationContext context) {
-    List<Waypoint> waypoints =
-        src.getWaypointsList().stream()
-            .map(
-                waypoint ->
-                    Waypoint.newBuilder()
-                        .setTripId(waypoint.getTripId())
-                        .setWaypointType(
-                            WaypointUtils.getWaypointTypeString(waypoint.getWaypointType()))
-                        .setLocation(
-                            SerializationUtils.createSerializedLocation(
-                                waypoint.getLocation().getPoint()))
-                        .build())
-            .collect(toList());
+    List<Waypoint> waypoints = src.getWaypointsList().stream()
+        .map(
+            waypoint -> Waypoint.newBuilder()
+                .setTripId(waypoint.getTripId())
+                .setWaypointType(
+                    WaypointUtils.getWaypointTypeString(waypoint.getWaypointType()))
+                .setLocation(
+                    SerializationUtils.createSerializedLocation(
+                        waypoint.getLocation().getPoint()))
+                .build())
+        .collect(toList());
 
-    SerializedVehicle vehicle =
-        SerializedVehicle.newBuilder()
-            .setName(src.getName())
-            .setVehicleState(src.getVehicleState().name())
-            .setCurrentTripsIds(src.getCurrentTripsList())
-            .setWaypoints(waypoints)
-            .setBackToBackEnabled(src.getBackToBackEnabled())
-            .setSupportedTripTypes(src.getSupportedTripTypesList())
-            .setMaximumCapacity(src.getMaximumCapacity())
-            .build();
+    SerializedLatLng serializedLatLng = SerializedLatLng.newBuilder()
+        .setLatitude(src.getLastLocation().getLocation().getLatitude())
+        .setLongitude(src.getLastLocation().getLocation().getLongitude())
+        .build();
+
+    SerializedLastLocation.Builder serializedLastLocationBuilder = SerializedLastLocation.newBuilder()
+        .setLatLng(serializedLatLng);
+
+    if (src.getLastLocation().hasHeading()) {
+      serializedLastLocationBuilder.setHeading(src.getLastLocation().getHeading().getValue());
+    } else {
+      serializedLastLocationBuilder.setHeading(0);
+    }
+    SerializedLastLocation serializedLastLocation = serializedLastLocationBuilder.build();
+
+    SerializedVehicle vehicle = SerializedVehicle.newBuilder()
+        .setName(src.getName())
+        .setVehicleState(src.getVehicleState().name())
+        .setCurrentTripsIds(src.getCurrentTripsList())
+        .setWaypoints(waypoints)
+        .setBackToBackEnabled(src.getBackToBackEnabled())
+        .setSupportedTripTypes(src.getSupportedTripTypesList())
+        .setMaximumCapacity(src.getMaximumCapacity())
+        .setLastLocation(serializedLastLocation)
+        .setVehicleAttributes(src.getAttributesList())
+        .setVehicleType(src.getVehicleType())
+        .build();
 
     return new Gson().toJsonTree(vehicle);
   }
